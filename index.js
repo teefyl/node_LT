2035197cdf86db4fb562b699a04b04331a561acd
teefyl <teefyl@tcd.ie>
2019-02-06 11:38:35 +0000

    Adding index files

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require ('express');
+const app = express();
+
+app.get('/', (req,res)=>{
+  res.sendFile(__dirname+'/public/index.html');
+});
+
+app.listen(3002);
+console.log("Port 3002 is open");
